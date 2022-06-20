9755cb6e92eb953cba2518afbeb616cb2e4719e4
David Greengas <dave.greengas@gmail.com>
2022-06-20 14:21:28 -0500

    added ultra plain http request

--- lib/src/features/upload_file/data/upload_respository.dart
+++ lib/src/features/upload_file/data/upload_respository.dart
@@ -1,8 +1,12 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:file_selector/file_selector.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http_parser/http_parser.dart';
 import 'package:http/http.dart' as http;
+import 'package:uuid/uuid.dart';
 
 abstract class UploadRepository {
   Future<int> uploadFile(XFile file);
@@ -48,6 +52,43 @@ class PlainHttpUploadRepository extends UploadRepository {
   }
 }
 
+class UltraPlainHttpUploadRepository extends UploadRepository {
+  UltraPlainHttpUploadRepository({required this.client});
+
+  final http.Client client;
+
+  @override
+  Future<int> uploadFile(XFile file) async {
+    var uri =
+        Uri.parse('http://localhost:25478/upload?token=f9403fc5f537b4ab332d');
+    final boundary = const Uuid().v4();
+    //const boundary = '----------------------c39ace087dfe7437';
+
+    final body = '''--$boundary
+Content-Disposition: form-data; name="file"; filename="${file.name}"
+Content-Type: ${file.mimeType ?? 'application/octet-stream'}
+
+${base64Encode(await file.readAsBytes())}
+--$boundary
+Content-Disposition: form-data; name="key"
+Content-Type: text/plain;charset=UTF-8
+
+text encoded in UTF-8
+--$boundary--
+       ''';
+
+    print(body);
+    final header = {'Content-Type': 'multipart/form-data;boundary="$boundary"'};
+    print(header);
+    var request = http.Request('POST', uri)
+      ..headers.addAll(header)
+      ..body = body;
+
+    var response = await client.send(request);
+    return response.statusCode;
+  }
+}
+
 final uploadRepositoryProvider = Provider<UploadRepository>((ref) {
-  return PlainHttpUploadRepository(client: http.Client());
+  return UltraPlainHttpUploadRepository(client: http.Client());
 });
